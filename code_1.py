8f189b50853c1f9371b837eae575e92e010f979f
genfilweb <142978168+genfilweb@users.noreply.github.com>
2024-10-03 20:32:15 +0200

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,14 @@
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+
+if __name__ == "__main__":
+
+
+    for item in data:
+import random
+
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+    main()
+    return data
